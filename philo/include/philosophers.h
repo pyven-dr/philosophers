af5b4c2bcc0ad6360d384367ac7268441e5c86c9
pyven-dr <pyven-dr@student.42lyon.fr>
2024-05-27 00:34:25 +0200

    change s_philo with an int tab to store values and define values to acess it

--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -15,4 +15,17 @@
 
 #include <unistd.h>
 
+# define NB_PHILO 0
+# define TIME_TO_DIE 1
+# define TIME_TO_EAT 2
+# define TIME_TO_SLEEP 3
+# define NB_TIME_MUST_EAT 4
+
+typedef struct s_philo
+{
+	int	philo_values[5];
+}	t_philo;
+
+int	parsing(int argc, char **argv, t_philo *philo_struct);
+
 #endif
